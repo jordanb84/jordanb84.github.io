4f18c4d5b5f9f3f5366c65ab6be3135cd2e957fd
Jordan <jhouse112233@gmail.com>
2017-01-24 17:57:18 -0600

    Updated edges

--- cubed/avatarmaker.js
+++ cubed/avatarmaker.js
@@ -13,26 +13,50 @@ window.onload = function() {
     input.addEventListener('change', handleFiles, false);
 }
 
+//  var imgd = context.getImageData(0, 0, 215, 220),
+
+//  context.putImageData(imgd, 0, 0);
+
+transparency = function() {
+
+  var d = context.getImageData(0, 0, 215, 220);
+  var L = d.length;
+
+    for ( var i = 3; i < L; i += 4 )
+    {
+        d[i] = 50;
+    }
+
+    context.putImageData(imgd, 0, 0);
+
+}
+
+
+
 function handleFiles(e) {
     var url = windowURL.createObjectURL(e.target.files[0]);
     var img = new Image();
     img.onload = function() {
 
-      context.drawImage(img, -20, 0, 235, 272);
+      context.drawImage(img, -22, -28, 235, 272);
 
       var outline = new Image();
       outline.src = ("3wa9d39d2.png");
       outline.onload = function (){
-        context.drawImage(outline, -20, 0);
+        context.drawImage(outline, -22, -28);
       }
 
       var cubedImage = new Image();
       cubedImage.src = ("3wa9d39d.png");
       cubedImage.onload = function (){
-        context.drawImage(cubedImage, -20, 0);
+        context.drawImage(cubedImage, -22, -28);
 
       }
+
     }
     img.src = url;
+
+    transparency();
+
 
 }
